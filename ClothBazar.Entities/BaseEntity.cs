26c0947feff9a7ccf7829f6e8f3b590b3246baf9
Gurinder Singh <37776221+guri1497@users.noreply.github.com>
2021-05-18 13:51:25 +0530

    entity added

---
+++ ClothBazar.Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+    public class BaseEntity // base class for all other classes for write common code 
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ClothBazar.Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+    public class Category : BaseEntity // inherit all the members and propertys of base class
+    {
+        public List<Product> Products { get; set; }
+    }
+}
