f15a0c25e665c207edef8bc7f00024a23080b52b
Gurinder Singh <37776221+guri1497@users.noreply.github.com>
2021-05-18 15:33:39 +0530

    entity framework install and generate database table with code first approach

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,20 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Database
+{
+    public class CBContext : DbContext //inherit for make our entity database
+    {
+        public CBContext():base("ClothBazarConnection") // tell dbcontext file which connection is use
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; } // individullay create database table
+        public DbSet<Product> Products { get; set; } // individullay create database table
+    }
+}
