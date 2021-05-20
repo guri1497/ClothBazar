bbca372079fdc37add15408c4757c9037b158ce5
Gurinder Singh <37776221+guri1497@users.noreply.github.com>
2021-05-21 01:19:12 +0530

    upload images in category and also working with two model in single way

--- ClothBazar.Web/Controllers/HomeController.cs
+++ ClothBazar.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClothBazar.Services;
+using ClothBazar.Web.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,13 @@ namespace ClothBazar.Web.Controllers
 {
     public class HomeController : Controller
     {
+        CategoriesService categoriesService = new CategoriesService(); // obj for use all services
+        HomeViewModel homeViewModel = new HomeViewModel();
+        
         public ActionResult Index()
         {
-            return View();
+            homeViewModel.Categories = categoriesService.GetCategories();
+            return View(homeViewModel);
         }
 
         public ActionResult About()
